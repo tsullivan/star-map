1f4239e1736f2857c6c68b0122b2484ff02c4560
Tim Sullivan <tsullivan@paypal.com>
2014-03-02 22:49:02 -0700

    nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,34 @@ var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	less = require('gulp-less'),
 	path = require('path'),
+	nodemon = require('nodemon'),
 	lr;
+
+// helper functions
+function startnodemon () {
+	// start the express app
+	nodemon({
+		script: 'index.js',
+		ext: 'js json'
+	});
+
+	// log nodemon events
+	nodemon.on('start', function () {
+		console.log('App has started');
+	})
+	.on('quit', function () {
+		console.log('App has quit');
+	})
+	.on('restart', function (files) {
+		console.log('App restarted due to: ', files);
+	});
+}
+
+function startlr () {
+	log('Starting Livereload');
+	lr = require('tiny-lr')();
+	lr.listen(35729);
+}
 
 function notifylr (event) {
 	var filename = path.relative(__dirname, event.path);
@@ -14,12 +41,7 @@ function notifylr (event) {
 	});
 }
 
-gulp.task('startlr', function () {
-	log('Starting Livereload');
-	lr = require('tiny-lr')();
-	lr.listen(35729);
-});
-
+// gulp tasks
 gulp.task('lint', function() {
 	gulp.src(['gulpfile.js', './src/*.js'])
 		.pipe(jshint({
@@ -45,7 +67,9 @@ gulp.task('watch', function () {
 	gulp.watch(['./less/**/*.less', './views/index.jade'], notifylr);
 });
 
-gulp.task('default', ['lint', 'less', 'watch', 'startlr'], function () {
-	// start express
-	require('./');
+// default task
+gulp.task('default', ['lint', 'less', 'watch'], function () {
+	// start dev servers
+	startlr();
+	startnodemon();
 });
