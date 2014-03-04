c9cb82d1b4c29a223f1b96ef37da476996920c05
Tim Sullivan <tsullivan@paypal.com>
2014-03-03 23:24:04 -0700

    add mongoskin

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/hiyagl", { native_parser: true });
+
+// star data from https://github.com/astronexus/HYG-Database/blob/master/hygxyz.csv
+db.bind('hygxyz');
+
 /*
  * Application routes
  */
@@ -8,5 +14,22 @@ module.exports = function (app) {
 	app.get('/', function(req, res){
 		res.render('index', { title: 'Star Map' });
 	});
+
+	/*
+	 * GET star data.
+	 */
+	app.get('/star-data', function(req, res){
+		db.hygxyz.findOne(function(err, item) {
+			if (err) {
+				res.json(500, { error: err.message || err });
+				console.log(err);
+			}
+			else {
+				res.json(item);
+			}
+		});
+
+	});
 }
 
+db.close();
